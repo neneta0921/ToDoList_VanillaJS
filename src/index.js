711da30bdd601558ded3200687a79b56437e553f
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-11-13 17:09:51 +0900

    Done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 const inputText = document.getElementById("textNewTodo");
 const todoItemsEl = document.querySelector(".todo-items");
-// const completeItemsEl = document.querySelector(".complete-items");
+const completeItemsEl = document.querySelector(".complete-items");
 const addBtn = document.getElementById("add-btn");
-// const completeBtn = document.querySelector(".complete-button");
-// const deleteBtn = document.querySelector(".delete-button");
-// const returnBtn = document.querySelector("return-btn");
-const todoList = [];
 
-// Create TODO DOM
-function createNewTodoDOM(text) {
+let todoListArray = [];
+let completeListArray = [];
+
+// Move to Complete Container
+function goToCompleteList(index) {
+  const moveList = todoListArray[index];
+  // Delete from todoListArray
+  todoListArray = todoListArray.filter((todoList) => todoList !== moveList);
+  // Add to completeListArray
+  completeListArray.push(moveList);
+  localStorage.setItem("todoListArray", JSON.stringify(todoListArray));
+  localStorage.setItem("completeListArray", JSON.stringify(completeListArray));
+  // Update DOM
+  todoItemsEl.textContent = "";
+  completeItemsEl.textContent = "";
+  showTodoLists();
+}
+
+// Delete TODO Item
+function deleteTodoItem(index) {
+  const deleteList = todoListArray[index];
+  // Delete from todoListArray
+  todoListArray = todoListArray.filter((todoList) => todoList !== deleteList);
+  localStorage.setItem("todoListArray", JSON.stringify(todoListArray));
+  // Update DOM
+  todoItemsEl.textContent = "";
+  showTodoLists();
+}
+
+// Create NEW TODO DOM
+function createNewTodoDOM(text, index) {
   // Create new Todo item
   const newTodo = document.createElement("li");
   newTodo.classList.add("list-group-item");
@@ -20,46 +45,102 @@ function createNewTodoDOM(text) {
   completeBtn.type = "button";
   completeBtn.classList.add("complete-btn");
   completeBtn.textContent = "完了";
+  completeBtn.setAttribute("onclick", `goToCompleteList(${index})`);
   const deleteBtn = document.createElement("button");
   deleteBtn.type = "button";
   deleteBtn.classList.add("delete-btn");
   deleteBtn.textContent = "削除";
+  deleteBtn.setAttribute("onclick", `deleteTodoItem(${index})`);
   // Append
   newTodo.append(todoText, completeBtn, deleteBtn);
   todoItemsEl.appendChild(newTodo);
-  // Delete Text
-  inputText.value = "";
 }
 
-// Save Todo Items to Local Storage
-function saveToLocalStorage(text, state) {
-  const saveTodoList = {
-    name: text,
-    state: state
-  };
-  todoList[todoList.length] = saveTodoList;
-  localStorage.setItem("todoLists", JSON.stringify(todoList));
+// Delete Complete TODO Item
+function deleteCompleteTodoItem(index) {
+  const deleteList = completeListArray[index];
+  // Delete from todoListArray
+  completeListArray = completeListArray.filter(
+    (completeList) => completeList !== deleteList
+  );
+  localStorage.setItem("completeListArray", JSON.stringify(completeListArray));
+  // Update DOM
+  completeItemsEl.textContent = "";
+  showTodoLists();
 }
 
-// Create Todo Item, save local storage
-function createNewTodoItem(text, state) {
-  createNewTodoDOM(text);
-  saveToLocalStorage(text, state);
+// Return todo items Container
+function returnTodoList(index) {
+  const moveList = completeListArray[index];
+  // Delete from completeListArray
+  completeListArray = completeListArray.filter(
+    (completeList) => completeList !== moveList
+  );
+  // Add to completeListArray
+  todoListArray.push(moveList);
+  localStorage.setItem("todoListArray", JSON.stringify(todoListArray));
+  localStorage.setItem("completeListArray", JSON.stringify(completeListArray));
+  // Update DOM
+  todoItemsEl.textContent = "";
+  completeItemsEl.textContent = "";
+  showTodoLists();
+}
+
+// Create Complete TODO DOM
+function createCompleteTodoDOM(text, index) {
+  // Create Complete Todo item
+  const completeTodo = document.createElement("li");
+  completeTodo.classList.add("list-group-item");
+  const todoText = document.createElement("span");
+  todoText.textContent = text;
+  todoText.classList.add("todo-item");
+  // Create Buttons
+  const returnBtn = document.createElement("button");
+  returnBtn.type = "button";
+  returnBtn.classList.add("return-btn");
+  returnBtn.textContent = "戻る";
+  returnBtn.setAttribute("onclick", `returnTodoList(${index})`);
+  const deleteBtn = document.createElement("button");
+  deleteBtn.type = "button";
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.textContent = "削除";
+  deleteBtn.setAttribute("onclick", `deleteCompleteTodoItem(${index})`);
+  // Append
+  completeTodo.append(todoText, returnBtn, deleteBtn);
+  completeItemsEl.appendChild(completeTodo);
 }
 
 // Create New Todo Item
 function createNewTodo() {
-  createNewTodoItem(inputText.value, "unfinished");
+  const text = inputText.value;
+  let todoListIndex = 0;
+  // Add Item to todoListArray
+  todoListArray.push(text);
+  // Get Index from todoListArray
+  todoListArray.forEach((item, index) => {
+    if (item === text) {
+      todoListIndex = index;
+    }
+  });
+  createNewTodoDOM(text, todoListIndex);
+  // Save Todo List to Local Storage
+  localStorage.setItem("todoListArray", JSON.stringify(todoListArray));
+  // Delete InputBox Text
+  inputText.value = "";
 }
 
 // Check LocalStorage, if todo list exits, show todo items
 function showTodoLists() {
-  if (localStorage.getItem("todoLists")) {
-    JSON.parse(localStorage.getItem("todoLists")).forEach((item) => {
-      todoList.push(item);
+  if (localStorage.getItem("todoListArray")) {
+    todoListArray = JSON.parse(localStorage.getItem("todoListArray"));
+    todoListArray.forEach((todoList, index) => {
+      createNewTodoDOM(todoList, index);
     });
-    todoList.forEach((item) => {
-      createNewTodoDOM(item.name);
+  }
+  if (localStorage.getItem("completeListArray")) {
+    completeListArray = JSON.parse(localStorage.getItem("completeListArray"));
+    completeListArray.forEach((completeList, index) => {
+      createCompleteTodoDOM(completeList, index);
     });
   }
 }
